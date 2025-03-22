89b378242dadb76f26411212b883e65b779beaf4
i3oi3ka <i3oi3ka@ukr.net>
2025-03-23 00:03:13 +0200

    add axios

--- src/js/consts.js
+++ src/js/consts.js
@@ -7,9 +7,13 @@ const refs = {
   loader: document.querySelector('.loader'),
 };
 
+const photosParams = {
+  page: 1,
+  perPage: 12,
+  maxPage: 1,
+};
+
 const KEY = '49310743-65b3f3ce2dd3324f0da11ae2e';
-const BASE_URL = 'https://pixabay.com';
-const ENDPOINT = 'api';
 const ACTIVE_CLASS = 'active';
 
 const lightbox = new SimpleLightbox('.js-gallery a', {
@@ -17,4 +21,4 @@ const lightbox = new SimpleLightbox('.js-gallery a', {
   captionDelay: 250, // Delay in milliseconds before showing the caption
 });
 
-export { refs, KEY, BASE_URL, ENDPOINT, ACTIVE_CLASS, lightbox };
+export { refs, KEY, ACTIVE_CLASS, photosParams, lightbox };
--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,19 +1,17 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-import {
-  refs,
-  BASE_URL,
-  ENDPOINT,
-  KEY,
-  ACTIVE_CLASS,
-  lightbox,
-} from './consts';
+import { refs, KEY, ACTIVE_CLASS, photosParams, lightbox } from './consts';
 import { createCardsMarkup } from './render-functions';
+import axios from 'axios';
 
-const handlerForm = event => {
+axios.defaults.baseURL = 'https://pixabay.com';
+
+async function handlerForm(event) {
   event.preventDefault();
   const form = event.currentTarget;
   const query = form.elements.request.value.trim();
+  photosParams.page = 1;
+  photosParams.maxPage = 1;
   if (query === '') {
     iziToast.show({
       title: 'Error',
@@ -22,46 +20,37 @@ const handlerForm = event => {
     });
     return;
   }
+  photosParams.q = query;
   refs.loader.classList.add(ACTIVE_CLASS);
-  getPhotos(query)
-    .then(photos => {
-      refs.loader.classList.remove(ACTIVE_CLASS);
-      if (photos.total === 0) {
-        iziToast.show({
-          title: 'Error',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          position: 'topRight',
-        });
-        refs.gallery.innerHTML = '';
-        return;
-      }
-      refs.gallery.innerHTML = createCardsMarkup(photos.hits);
-      lightbox.refresh();
-    })
-    .catch(err => {
-      refs.loader.classList.remove(ACTIVE_CLASS);
-      console.log(err);
-    })
-    .finally(() => {
-      form.reset();
-    });
-};
-
-function getPhotos(query) {
-  const SearchParams = new URLSearchParams({
-    key: KEY,
-    q: query,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  });
-  return fetch(`${BASE_URL}/${ENDPOINT}/?${SearchParams}`).then(res => {
-    if (!res.ok) {
-      throw new Error(res.status);
+  try {
+    const photos = await getPhotos(photosParams);
+    refs.loader.classList.remove(ACTIVE_CLASS);
+    if (photos.total === 0) {
+      iziToast.show({
+        title: 'Error',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topRight',
+      });
+      refs.gallery.innerHTML = '';
+      return;
     }
-    return res.json();
+    photosParams.maxPage = Math.ceil(photos.totalHits / photosParams.perPage);
+    refs.gallery.innerHTML = createCardsMarkup(photos.hits);
+    lightbox.refresh();
+  } catch (err) {
+    refs.loader.classList.remove(ACTIVE_CLASS);
+    console.log(err);
+  } finally {
+    form.reset();
+  }
+}
+
+async function getPhotos({ q, page = 1, perPage = 12 }) {
+  const res = await axios.get('api/', {
+    params: { key: KEY, q, page, per_page: perPage },
   });
+  return res.data;
 }
 
 export { handlerForm };
