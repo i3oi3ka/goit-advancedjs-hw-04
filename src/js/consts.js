07ed8556241ebd774695fb14235cc1d0c131384c
i3oi3ka <i3oi3ka@ukr.net>
2025-03-25 01:30:47 +0200

    complete HW

--- src/js/consts.js
+++ src/js/consts.js
@@ -5,20 +5,22 @@ const refs = {
   form: document.querySelector('.js-search-form'),
   gallery: document.querySelector('.js-gallery'),
   loader: document.querySelector('.loader'),
+  loadMoreBtn: document.querySelector('.js-loadMore-btn'),
 };
 
 const photosParams = {
   page: 1,
-  perPage: 12,
+  perPage: 15,
   maxPage: 1,
 };
 
 const KEY = '49310743-65b3f3ce2dd3324f0da11ae2e';
 const ACTIVE_CLASS = 'active';
+const HIDDEN_CLASS = 'is-hidden';
 
 const lightbox = new SimpleLightbox('.js-gallery a', {
   captionsData: 'alt', // Use the alt attribute of images for captions
   captionDelay: 250, // Delay in milliseconds before showing the caption
 });
 
-export { refs, KEY, ACTIVE_CLASS, photosParams, lightbox };
+export { refs, KEY, ACTIVE_CLASS, HIDDEN_CLASS, photosParams, lightbox };
--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,6 +1,13 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-import { refs, KEY, ACTIVE_CLASS, photosParams, lightbox } from './consts';
+import {
+  refs,
+  KEY,
+  ACTIVE_CLASS,
+  HIDDEN_CLASS,
+  photosParams,
+  lightbox,
+} from './consts';
 import { createCardsMarkup } from './render-functions';
 import axios from 'axios';
 
@@ -9,9 +16,10 @@ axios.defaults.baseURL = 'https://pixabay.com';
 async function handlerForm(event) {
   event.preventDefault();
   const form = event.currentTarget;
-  const query = form.elements.request.value.trim();
+  refs.loadMoreBtn.classList.add(HIDDEN_CLASS);
   photosParams.page = 1;
-  photosParams.maxPage = 1;
+  const query = form.elements.request.value.trim();
+
   if (query === '') {
     iziToast.show({
       title: 'Error',
@@ -20,6 +28,7 @@ async function handlerForm(event) {
     });
     return;
   }
+
   photosParams.q = query;
   refs.loader.classList.add(ACTIVE_CLASS);
   try {
@@ -37,7 +46,13 @@ async function handlerForm(event) {
     }
     photosParams.maxPage = Math.ceil(photos.totalHits / photosParams.perPage);
     refs.gallery.innerHTML = createCardsMarkup(photos.hits);
+
     lightbox.refresh();
+
+    if (photosParams.maxPage !== 1) {
+      refs.loadMoreBtn.classList.remove(HIDDEN_CLASS);
+      refs.loadMoreBtn.addEventListener('click', handleLoadMore);
+    }
   } catch (err) {
     refs.loader.classList.remove(ACTIVE_CLASS);
     console.log(err);
@@ -46,7 +61,38 @@ async function handlerForm(event) {
   }
 }
 
-async function getPhotos({ q, page = 1, perPage = 12 }) {
+async function handleLoadMore(event) {
+  refs.loadMoreBtn.disabled = true;
+  refs.loader.classList.add(ACTIVE_CLASS);
+  photosParams.page += 1;
+  try {
+    const photos = await getPhotos(photosParams);
+    refs.loader.classList.remove(ACTIVE_CLASS);
+    refs.gallery.insertAdjacentHTML(
+      'beforeend',
+      createCardsMarkup(photos.hits)
+    );
+    const cardHeight = refs.gallery
+      .querySelector('li')
+      .getBoundingClientRect().height;
+    window.scrollBy({ top: cardHeight * 2, left: 0, behavior: 'smooth' });
+    lightbox.refresh();
+    refs.loadMoreBtn.disabled = false;
+    if (photosParams.page >= photosParams.maxPage) {
+      refs.loadMoreBtn.classList.add(HIDDEN_CLASS);
+      refs.loadMoreBtn.removeEventListener('click', handleLoadMore);
+      iziToast.show({
+        message: "We're sorry, but you've reached the end of search results.",
+        position: 'topRight',
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    refs.loader.classList.remove(ACTIVE_CLASS);
+  }
+}
+
+async function getPhotos({ q, page = 1, perPage = 15 }) {
   const res = await axios.get('api/', {
     params: { key: KEY, q, page, per_page: perPage },
   });
